5e9fbaecd8e0e1ee642e3f1c5e2f7cd6170e89cc
sohailxdev <100mohdsohail@gmail.com>
2025-06-08 19:31:26 +0530

    cors allow all

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,3 @@
-
 import express, { urlencoded } from "express";
 import dotenv from "dotenv";
 import { Rooms } from "./models/Rooms.models.js";
@@ -7,49 +6,35 @@ import cors from "cors";
 import roomRouter from "./routes/room.routes.js";
 import userRouter from "./routes/user.routes.js";
 import sellerRouter from "./routes/seller.routes.js";
-import { v2 as cloudinary } from 'cloudinary';
-import multer from 'multer';
+import { v2 as cloudinary } from "cloudinary";
+import multer from "multer";
 import reviewRouter from "./routes/review.routes.js";
-
 
 dotenv.config();
 
 connectDB();
 
-
-cloudinary.config({ 
-    api_key: process.env.api_key, 
-    cloud_name: process.env.cloud_name, 
-    api_secret: process.env.api_secret 
+cloudinary.config({
+  api_key: process.env.api_key,
+  cloud_name: process.env.cloud_name,
+  api_secret: process.env.api_secret,
 });
-
-
 
 const storage = multer.memoryStorage();
 export const upload = multer({ storage });
 
-
-
 const app = express();
 
+app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"] }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(cors());
-app.use(express.urlencoded({extended: true}));
-app.use(express.json())
-
-
-
-
-
-app.use("/api/v1/room" , roomRouter);
-app.use("/api/v1/user" , userRouter);
-app.use("/api/v1/seller" , sellerRouter); 
-app.use("/api/v1/review" , reviewRouter);
-
-
-
+app.use("/api/v1/room", roomRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/seller", sellerRouter);
+app.use("/api/v1/review", reviewRouter);
 
 const port = process.env.PORT || 8000;
-app.listen(port , () => {
-    console.log(`listening on port ${port} ...`);
+app.listen(port, () => {
+  console.log(`listening on port ${port} ...`);
 });
